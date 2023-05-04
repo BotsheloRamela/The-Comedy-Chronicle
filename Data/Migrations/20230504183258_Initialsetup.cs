b46ce8f1950c09f4c99ddb62c1b3021ddde842bc
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-05-04 20:33:51 +0200

    feat(data): Added Joke migration

---
+++ Data/Migrations/20230504183258_Initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace The_Comedy_Chronicle.Data.Migrations
+{
+    public partial class Initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    jokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    jokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
