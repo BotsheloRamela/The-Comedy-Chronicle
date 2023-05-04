cb68b7d2e21d987b5a734b279a613c84708ed083
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-05-04 20:24:33 +0200

    refactor(backend): Added Joke class

---
+++ Models/Joke.cs
@@ -0,0 +1,11 @@
+﻿namespace The_Comedy_Chronicle.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string jokeQuestion { get; set; }
+
+        public  string jokeAnswer { get; set; }
+    }
+}
