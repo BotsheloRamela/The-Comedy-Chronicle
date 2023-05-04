084262ff31fcfb687b79de024515ed6d1380775d
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-05-04 20:08:10 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace The_Comedy_Chronicle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
